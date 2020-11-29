6af13f0e9d8e91a101347c6ab179d91f01f5bd57
semyers189 <semyers@fullsail.edu>
2020-11-28 20:36:36 -0500

    added main dad joke

---
+++ bot.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+client.on('ready', () => {
+  console.log('Bot is ready!')
+})
+
+client.on('message', (message) => {
+  if (message.author.bot) {
+    return;
+  }
+  const lowMessage = message.content.toLowerCase();
+  if (lowMessage.indexOf("i'm ") !== -1) {
+    const begin = lowMessage.indexOf("i'm ");
+    const jokeNoun = lowMessage.substr(begin).split(' ');
+    message.channel.send(`Hiya ${jokeNoun[1]}, I'm a Robit`);
+  }
+  if (lowMessage.indexOf("im ") !== -1) {
+    const begin = lowMessage.indexOf("im ");
+    const jokeNoun = lowMessage.substr(begin).split(' ');
+    message.channel.send(`Hiya ${jokeNoun[1]}, I'm a Robit`);
+  }
+  if (lowMessage.indexOf("i am ") !== -1) {
+    const begin = lowMessage.indexOf("i am ");
+    const jokeNoun = lowMessage.substr(begin).split(' ');
+    message.channel.send(`Hiya ${jokeNoun[2]}, I'm a Robit`);
+  }
+})
