a30dff77ef8d0e2e63ce663d002aa0a437c6c206
semyers189 <semyers@fullsail.edu>
2020-11-28 20:58:48 -0500

    fixed file path error

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public.index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 })
 
 app.listen(port, () => {
