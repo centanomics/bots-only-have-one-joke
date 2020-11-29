b4302889a40d1aaaad045513250343ea26a466a6
semyers189 <semyers@fullsail.edu>
2020-11-28 20:56:44 -0500

    added basic html page

--- bot.js
+++ bot.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public.index.html'));
+})
+
 app.listen(port, () => {
   console.log(`Birbit listening on port ${port}`)
 })
