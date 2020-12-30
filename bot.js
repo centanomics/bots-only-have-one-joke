b75ed4fb3125672ced0044e3cffe6f6d625f9116
semyers189 <semyers@fullsail.edu>
2020-12-30 14:37:33 -0500

    changed the finding noun stuffs for the better

--- bot.js
+++ bot.js
@@ -23,30 +23,51 @@ client.on('ready', () => {
 })
 
 client.on('message', (message) => {
-  if (message.author.bot) {
-    return;
-  }
-  const messegeRegex = /([‘’])/g;
-  let lowMessage = message.content.toLowerCase();
-  lowMessage = lowMessage.replace(messegeRegex, '\'');
-  const regex = /(i'm|im|i am)/g;
-  const returnregex = lowMessage.match(regex);
-
-  const firstWord =
-  returnregex === null ? null :
-  returnregex[0].split(' ')[1] === 'am' ? 'i am' :
-  returnregex[0].split(' ')[0];
-  const charBefore = lowMessage.charAt(lowMessage.indexOf(returnregex[0]) - 1);
-  console.log(`\nregex: ${returnregex[0]}\nfirst word: ${firstWord}\nchar before: ${charBefore}`);
-  if (returnregex !== null) {
-    if (firstWord !== 'i am' && firstWord !== 'im' && firstWord !== 'i\'m') {
-      return
-    } else if (charBefore !== ' ' && charBefore !== '') {
-      return
-    } else {
-      const noun = returnregex[0].substring(returnregex[0].lastIndexOf(' ') + 1);
-      message.channel.send(`Hiya ${noun}, I'm a Robit`);
+  try {
+    if (message.author.bot) {
+      return;
     }
-  }
+    const messegeRegex = /([‘’])/g;
+    let lowMessage = message.content.toLowerCase();
+    lowMessage = lowMessage.replace(messegeRegex, '\'');
+    const regex = /(i'm|im|i am)/g;
+    const returnregex = lowMessage.match(regex);
 
+    const firstWord =
+      returnregex === null ? [null] :
+        returnregex[0].split(' ')[1] === 'am' ? ['i am'] :
+          lowMessage.split(' ').filter(word => (word.indexOf(returnregex[0]) !== -1));
+    // console.log(lowMessage.split(' ').filter((word) => word.indexOf(returnregex[0]) !== -1))
+    const charBefore = lowMessage.charAt(lowMessage.indexOf(returnregex[0]) - 1);
+    // console.log(
+    //   `\nregex: ${returnregex[0]}\nfirst word: ${firstWord[0]}\nchar before: ${charBefore}`
+    // );
+    if (returnregex !== null) {
+    
+      if (firstWord[0] !== 'i am' && firstWord[0] !== 'im' && firstWord[0] !== 'i\'m') {
+        return
+      } else if (charBefore !== ' ' && charBefore !== '') {
+        return
+      } else {
+        const ignoreArray = ['i\'m', 'im', 'i', 'am', 'a', 'not', 'the', 'an'];
+        const sentenceArray = lowMessage.split(' ');
+        // const start = sentenceArray.indexOf(returnregex) !== -1 ? sentenceArray.indexOf(returnregex) : sentenceArray.indexOf('am')
+        let noun = '';
+        console.log(sentenceArray.indexOf(returnregex))
+        for (let i = 0; i < sentenceArray.length; i++) {
+          // console.log(ignoreArray.indexOf(sentenceArray[i]) === -1)
+          if (ignoreArray.indexOf(sentenceArray[i]) === -1) {
+            noun = sentenceArray[i];
+            // console.log(sentenceArray[i])
+            break;
+          }
+          // message.channel.send(ignoreArray.indexOf(sentenceArray[i]) === -1)
+        }
+
+        message.channel.send(`Hiya ${noun}, I'm a Robit`);
+      }
+    }
+  } catch (err) {
+    console.log('no variation of I am found')
+  }
 })
