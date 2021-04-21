8e3e7b6a2f5e13e99eebd01cc26472f4053c2010
semyers189 <semyers@fullsail.edu>
2021-04-20 22:57:56 -0400

    added based trigger

--- bot.js
+++ bot.js
@@ -6,11 +6,11 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'));
-})
+});
 
 app.listen(port, () => {
-  console.log(`Birbit listening on port ${port}`)
-})
+  console.log(`Birbit listening on port ${port}`);
+});
 
 const Discord = require('discord.js');
 
@@ -19,42 +19,54 @@ const client = new Discord.Client();
 client.login(process.env.DISCORD_BOT_TOKEN);
 
 client.on('ready', () => {
-  console.log('Bot is ready!')
-})
+  console.log('Bot is ready!');
+});
 
 client.on('message', (message) => {
   try {
     if (message.author.bot) {
       return;
     }
+    if (message.content.toLowerCase().indexOf('based') !== -1) {
+      message.channel.send('Based on what?');
+    }
     const messegeRegex = /([‘’])/g;
     let lowMessage = message.content.toLowerCase();
-    lowMessage = lowMessage.replace(messegeRegex, '\'');
+    lowMessage = lowMessage.replace(messegeRegex, "'");
     const regex = /(i'm|im|i am)/g;
     const returnregex = lowMessage.match(regex);
 
     const firstWord =
-      returnregex === null ? [null] :
-        returnregex[0].split(' ')[1] === 'am' ? ['am'] :
-          lowMessage.split(' ').filter(word => (word.indexOf(returnregex[0]) !== -1));
+      returnregex === null
+        ? [null]
+        : returnregex[0].split(' ')[1] === 'am'
+        ? ['am']
+        : lowMessage
+            .split(' ')
+            .filter((word) => word.indexOf(returnregex[0]) !== -1);
     // console.log(lowMessage.split(' ').filter((word) => word.indexOf(returnregex[0]) !== -1))
-    const charBefore = lowMessage.charAt(lowMessage.indexOf(returnregex[0]) - 1);
+    const charBefore = lowMessage.charAt(
+      lowMessage.indexOf(returnregex[0]) - 1
+    );
     console.log(
       `\nregex: ${returnregex[0]}\nfirst word: ${firstWord[0]}\nchar before: ${charBefore}`
     );
     const checkReg = /(i'm|im|i am)( a? ?(robit|birbit))/g;
     if (lowMessage.match(checkReg) !== null) {
-      message.channel.send('No you\'re not.');
+      message.channel.send("No you're not.");
       return;
     }
     if (returnregex !== null) {
-    
-      if (firstWord[0] !== 'am' && firstWord[0] !== 'im' && firstWord[0] !== 'i\'m') {
-        return
+      if (
+        firstWord[0] !== 'am' &&
+        firstWord[0] !== 'im' &&
+        firstWord[0] !== "i'm"
+      ) {
+        return;
       } else if (charBefore !== ' ' && charBefore !== '') {
-        return
+        return;
       } else {
-        const ignoreArray = ['i\'m', 'im', 'i', 'am', 'a', 'not', 'the', 'an'];
+        const ignoreArray = ["i'm", 'im', 'i', 'am', 'a', 'not', 'the', 'an'];
         const sentenceArray = lowMessage.split(' ');
         const start = sentenceArray.indexOf(firstWord[0]);
         let noun = '';
@@ -69,7 +81,11 @@ client.on('message', (message) => {
         }
       }
     }
+
+    if (message.content.toLowerCase().indexOf('based') !== -1) {
+      message.channel.send('Based on what?');
+    }
   } catch (err) {
-    console.log('no variation of I am found')
+    console.log('no variation of I am found');
   }
-})
+});
